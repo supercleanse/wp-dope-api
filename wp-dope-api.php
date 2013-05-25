365ed3ab0cdbfb346b3399ef74265a7023e8c5d6
Blair Williams <supercleanse@gmail.com>
2013-05-24 22:42:49 -0600

    Rushed commit ... roughing in still

--- wp-dope-api.php
+++ wp-dope-api.php
@@ -13,6 +13,49 @@ Copyright: 2004-2013, Blair Williams
 if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');}
 
 class WpDopeApi() {
+  public function __construct() {
+    // Add initialization and activation hooks
+    add_action('init', array($this,'init'));
+  }
+
+  public function init() {
+    add_filter('rewrite_rules_array', array($this,'rewrites'));
+  }
+  
+  function rewrites($wp_rules) {
+    $base = get_option('base', 'api');
+    if (empty($base)) {
+      return $wp_rules;
+    }
+    $rules = array(
+      "$base\$" => 'index.php?json=info',
+      "$base/(.+)\$" => 'index.php?json=$matches[1]'
+    );
+    return array_merge($rules, $wp_rules);
+  }
+  
+  function dir() {
+    if (defined('JSON_API_DIR') && file_exists(JSON_API_DIR)) {
+      return JSON_API_DIR;
+    } else {
+      return dirname(__FILE__);
+    }
+  }
+
+  function wpdapi_activation() {
+    // Add the rewrite rule on activation
+    global $wp_rewrite;
+    add_filter('rewrite_rules_array', 'rewrites');
+    $wp_rewrite->flush_rules();
+  }
+  
+  function wpdapi_deactivation() {
+    // Remove the rewrite rule on deactivation
+    global $wp_rewrite;
+    $wp_rewrite->flush_rules();
+  }
+register_activation_hook("$dir/json-api.php", 'activation');
+register_deactivation_hook("$dir/json-api.php", 'deactivation');
 
 }
 
