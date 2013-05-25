d58eed7bc413b78a415a7388781e099273fc8b73
Blair Williams <supercleanse@gmail.com>
2013-05-24 22:31:03 -0600

    Boom

---
+++ wp-dope-api.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: WP Dope API
+Plugin URI: http://www.blairwilliams.com/
+Description: A dope example of creating a WordPress based API bro
+Version: 0.0.1
+Author: Blair Williams
+Author URI: http://blairwilliams.com/
+Text Domain: wp-dope-api
+Copyright: 2004-2013, Blair Williams
+*/
+
+if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');}
+
+class WpDopeApi() {
+
+}
+
+new WpDopeApi();
